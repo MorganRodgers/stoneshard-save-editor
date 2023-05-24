692fcfa23a7b380be66f84c6d7e5bf5e26d39b12
Morgan Rodgers <morgan.rodgers@gmail.com>
2023-05-23 19:56:19 -0500

    Moved config into an INI and improved messaging

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python3
 from pathlib import Path
 from typing import Dict
+import configparser
 import hashlib
 import json
+import sys
 import zlib
 
 
+def load_config() -> Dict:
+    config = {"character": {}, "inventory": {}, "filesystem": {}}
+
+    parser = configparser.ConfigParser()
+    parser.read("config.ini")
+
+    for section in config.keys():
+        for key, value in (parser[section] or {}).items():
+            config[section][key] = value
+
+    # If output file is not set don't clobber the input
+    if "output_save_file_path" not in config["filesystem"]:
+        config["filesystem"]["output_save_file_path"] = (
+            config["filesystem"]["input_save_file_path"] + ".new"
+        )
+    elif config["filesystem"]["output_save_file_path"] == "overwrite":
+        config["filesystem"]["output_save_file_path"] = config["filesystem"][
+            "input_save_file_path"
+        ]
+
+    return config
+
+
 def decompress_stoneshard_sav(sav_path: Path) -> Dict:
+    if not sav_path.exists():
+        print(f"Unable to find save file at {sav_path}")
+        sys.exit(1)
+
     content = sav_path.open("rb").read()
     decompressed_content = zlib.decompress(content)
     decoded_content = decompressed_content[:-33].decode("utf8")
@@ -33,8 +62,10 @@ def compress_stoneshard_sav(content: Dict, sav_path: Path) -> Dict:
         serialized_content.encode("utf8") + checksum + b"\x00"
     )
 
-    with Path("data.sav.altered").open("wb") as output_file:
+    with sav_path.open("wb") as output_file:
         output_file.write(compressed_content)
+
+    print(f"Updated save file written to {sav_path}")
 
 
 def mutate_character(save_content: Dict, character_config: Dict):
@@ -42,37 +73,33 @@ def mutate_character(save_content: Dict, character_config: Dict):
 
     for key, value in character_config.items():
         if key == "xp":
-            character["XP"] = value
+            character["XP"] = int(value)
         elif key == "ability_points":
-            character["AP"] = value
+            character["AP"] = int(value)
         elif key == "str":
-            character["STR"] = value
+            character["STR"] = int(value)
         elif key == "level":
-            character["LVL"] = value
+            character["LVL"] = int(value)
 
 
 def mutate_inventory(save_content: Dict, inventory_config: Dict):
     inventory = save_content["inventoryDataList"]
     for key, value, *ignored in inventory:
         if inventory_config["moneybag"] and key == "o_inv_moneybag":
-            value["Stack"] = inventory_config["moneybag"]
+            value["Stack"] = int(inventory_config["moneybag"])
 
 
 def main():
-    path = Path("character_1/exitsave_1/data.sav")
+    config = load_config()
 
-    inventory_config = {"moneybag": 1998}  # todo load from json
-    character_config = {
-        "xp": 60000,
-        "ability_points": 20,
-    }  # todo load from json
-
+    path = Path(config["filesystem"]["input_save_file_path"]).expanduser()
     save_content = decompress_stoneshard_sav(path)
 
-    mutate_character(save_content, character_config)
+    mutate_character(save_content, config["character"])
 
-    mutate_inventory(save_content, inventory_config)
+    mutate_inventory(save_content, config["inventory"])
 
+    path = Path(config["filesystem"]["output_save_file_path"]).expanduser()
     compress_stoneshard_sav(save_content, path)
 
 
